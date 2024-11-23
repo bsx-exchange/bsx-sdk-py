d1ab84991c1a1d3d62edd46266ab9219e53571c9
bsx-engineering <dev@bsx.exchange>
2024-11-23 18:13:30 +0700

    chore: update examples

--- main_async.py
+++ main_async.py
@@ -1,91 +1,115 @@
-import _thread
 import asyncio
-import base64
-import hashlib
-import hmac
-import threading
+from http import client
 import time
-from datetime import timedelta
-from decimal import Decimal
 
-import pandas
 from eth_account import Account
-from bsx_py import BSXInstance, Environment
-from bsx_py.common.types.account import WithdrawParams
-from bsx_py.common.types.market import *
+from bsx_py.common.types.market import (
+    BatchOrderUpdateParams,
+    CancelAllParams,
+    CancelOrderParams,
+    CreateOrderParams,
+    OrderType,
+    Side,
+)
+from bsx_py.instance import BSXInstance, Environment
 
 
-def new_order(b, n):
+async def create(bsx_instance: BSXInstance):
     try:
-        print(b.create_order(params=CreateOrderParams(
-            type=OrderType.LIMIT,
+        params = CreateOrderParams(
             side=Side.BUY,
-            product_index=3,
-            price=Decimal("156"),
-            size=Decimal("2.5"),
+            type=OrderType.LIMIT,
+            product_index=1,
+            price=80000,
+            size=0.1,
             time_in_force="GTC",
-            # nonce=n,
-            nonce=int(time.time() * 1e9),
-            post_only=True,
-        )))
+            nonce=int(time.time_ns()),
+        )
+        order = await bsx_instance.create_order_async(params)
+        print(order)
+    except Exception as e:
+        print(e)
+
+
+async def cancel(bsx_instance: BSXInstance):
+    try:
+        # cancel by id
+        result = await bsx_instance.cancel_order_async(
+            order_id="62bd5069-8450-4015-b7ca-abb034054cd0",
+        )
+        print(result)
+
+        # cancel all orders of a specific product
+        result = await bsx_instance.cancel_all_orders_async(product_id="BTC-PERP")
+        print(result)
+        pass
+    except Exception as e:
+        print(e)
+
+
+async def batch(bsx_instance: BSXInstance):
+    try:
+        order0 = await bsx_instance.create_order_async(
+            CreateOrderParams(
+                side=Side.BUY,
+                type=OrderType.LIMIT,
+                product_index=1,
+                price=80000,
+                size=0.1,
+                time_in_force="GTC",
+                nonce=int(time.time_ns()),
+            )
+        )
+        print(order0)
+        print("===")
+        result = await bsx_instance.batch_update_orders_async(
+            BatchOrderUpdateParams(
+                [
+                    CancelAllParams(product_id="BTC-PERP"),
+                    CreateOrderParams(
+                        side=Side.BUY,
+                        type=OrderType.LIMIT,
+                        product_index=1,
+                        price=79000,
+                        size=0.1,
+                        time_in_force="GTC",
+                        nonce=int(time.time_ns()),
+                    ),
+                ]
+            )
+        )
+        print(result)
     except Exception as e:
         print(e)
 
 
 async def main():
-    # wallet_private_key = "0x75ce163a131b85066a8c9c69ca209b5898062dad91fe1ca153750f4ca58dcd3e" # multi sig
-    # wallet_private_key = "0xb318e3cf7ae1abf758ae6d0b965298bcd87e6b49a9aabaae03606acc391b7e24" # empty acc
-    wallet_private_key = "0x497cb75972414c032715536153426c07ac59e8600b7883115de2d6edde72d4c4"  # eth maker 1
-    signer_private_key = "0xb318e3cf7ae1abf758ae6d0b965298bcd87e6b49a9aabaae03606acc391b7e24"
-    wallet = Account.from_key(wallet_private_key)
-    signer = Account.from_key(signer_private_key)
-    # bsx_instance = BSXInstance(env="https://api.dev.bsx.exchange", wallet=wallet, signer=signer)
-    bsx_instance = BSXInstance(env="http://localhost:8090", wallet=wallet, signer=signer)
+    bsx_instance = BSXInstance.from_api_key(
+        api_key="7de31802d6bbd54230db0c3ac0c996a7",
+        api_secret="ba1b31a16363a16916e695269d90925e3c5501d67fe89329be476c52bddd5a23",
+        signer=Account.from_key(
+            "0x746de15abd290263318d37e877c0c97e1138c5907eae7c5171b124039e77be2b"
+        ),
+        env=Environment.TESTNET,
+    )
 
-    # bsx_instance = BSXInstance.from_multi_sig_wallet(env="http://localhost:8090", contract=wallet, signer=signer, owner_address="0x9fF6E36Cf97A01b169a5e6E351ab1375955271A3")
-    # bsx_instance = BSXInstance.from_api_key(env="http://localhost:8090", api_key="764b24c65762509b2f85e5082264bfe1",
-    #                                         api_secret="79b9ff5dd151b6f4b72b5b309b7a1a0780316e18c5bf51b10c57b0bbc8764b1b",
-    #                                         signer=signer)
-    async def test1():
-        print(await bsx_instance.batch_update_orders_async(
-            params=BatchOrderUpdateParams(operations=[
-                # CreateOrderParams(
-                #     type=OrderType.LIMIT,
-                #     side=Side.BUY,
-                #     product_index=3,
-                #     price=Decimal("10"),
-                #     size=Decimal("2.5"),
-                #     time_in_force="GTC",
-                #     nonce=int(time.time_ns()),
-                #     post_only=False,
-                #     client_order_id="127"
-                # ),
-                CancelOrderParams(nonce="456"),
-                # CancelAllParams(product_id="BTC-PERP"),
-                # CancelOrdersParams(client_order_ids=['127'])
-            ])
-        ))
+    # NOTE: this is another way to init bsx client
+    # bsx_instance = BSXInstance(
+    #     env=Environment.TESTNET,
+    #     wallet=Account.from_key(
+    #         "0x75ce163a131b85066a8c9c69ca209b5898062dad91fe1ca153750f4ca58dcd3e"
+    #     ),
+    #     signer=Account.from_key(
+    #         "0xb318e3cf7ae1abf758ae6d0b965298bcd87e6b49a9aabaae03606acc391b7e24"
+    #     ),
+    # )
 
-    async def test2():
-        print(await bsx_instance.batch_update_orders_async(
-            params=BatchOrderUpdateParams(operations=[
-                # CreateOrderParams(
-                #     type=OrderType.LIMIT,
-                #     side=Side.BUY,
-                #     product_index=3,
-                #     price=Decimal("10"),
-                #     size=Decimal("2.5"),
-                #     time_in_force="GTC",
-                #     nonce=int(time.time_ns()),
-                #     post_only=False,
-                #     client_order_id="127"
-                # ),
-                CancelOrderParams(nonce="123"),
-                # CancelAllParams(product_id="BTC-PERP"),
-                # CancelOrdersParams(client_order_ids=['127'])
-            ])
-        ))
+    print("### create")
+    await create(bsx_instance=bsx_instance)
+    print("### cancel")
+    await cancel(bsx_instance=bsx_instance)
+    print("### batch")
+    await batch(bsx_instance=bsx_instance)
 
-    await asyncio.gather(test1(), test2())
 
 asyncio.run(main())
